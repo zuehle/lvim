1854ed3d9531515f026a3b1803d8a291fb06ae82
Johannes Uhlig <ju@juhlig.eu>
2023-08-23 09:35:19 +0200

    initial commit of fork

---
+++ config.lua
@@ -0,0 +1,24 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+-- additional plugins
+reload "user.plugins"
+reload "user.options"
+reload "user.keymaps"
+reload "user.autocommands"
+reload "user.lsp"
+reload "user.treesitter"
+--reload "user.copilot"
+reload "user.telescope"
+reload "user.fidget"
+reload "user.whichkey"
+--reload "user.inlay-hints"
+--reload "user.functions"
+--reload "user.dial"
+
+-- reload "user.chatgpt"
+-- reload "user.neoai"
+--
+-- https://github.com/adibhanna/lvim-config/tree/main
